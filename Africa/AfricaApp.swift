0299c81cbfa579dbf727a5cc7ed7ed7f764143ae
EvgeshkO35 <56804253+EvgeshkO35@users.noreply.github.com>
2021-04-12 12:49:52 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Evgenii Lysenko on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
